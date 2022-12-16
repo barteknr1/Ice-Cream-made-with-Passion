3031b43aa06f05190b8a4e2a93175795cfa756be
barteknr1 <114334919+barteknr1@users.noreply.github.com>
2022-12-16 15:56:44 +0000

    Deploying to gh-pages from @ barteknr1/Projekt-grupowy@cb3a341bce4e7a229b885aad1b02efa9948cb96c 🚀

--- src.a769d7b3.js
+++ src.a769d7b3.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/Projekt-grupowy/src.b32a31da.js.map
+//# sourceMappingURL=/Projekt-grupowy/src.a769d7b3.js.map
